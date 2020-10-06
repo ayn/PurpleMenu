323b2f954ac49771f5c0fdb5e43f621e308f2162
Andrew Ng <ayn@andrewng.com>
2020-10-06 11:27:56 -0700

    Initial Commit

---
+++ PurpleMenu/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  PurpleMenu
+//
+//  Created by Andrew Ng on 10/6/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
